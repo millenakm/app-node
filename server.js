92b9646e959f09e7e9d80b486031d56585da60e7
Millena Kupsinsku Martins <millena.martins@digitaldesk.com.br>
2017-03-09 13:44:35 -0300

    server application

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require('express');
-var app = express();
+var app = express();//contem os métodos do pacote express
 var port = 5000;
+
+var application = require('');//seleciona o arquivo da aplicação
+app.use(application);
 
 app.listen(port);
 console.log("Servidor iniciado na porta "+port+". Acesso em localhost:"+port);
