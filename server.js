07e912b6ba751b6a0373861a814a61448743fad7
Millena Kupsinsku Martins <millena.martins@digitaldesk.com.br>
2017-03-09 14:00:57 -0300

    rota para aplicação

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ var express = require('express');
 var app = express();//contem os métodos do pacote express
 var port = 5000;
 
-var application = require('');//seleciona o arquivo da aplicação
-app.use(application);
+var application = require('./app');//faz a chamada da aplicação
+app.use(application);//usa o app.js
 
-app.listen(port);
+app.listen(port);//seta isso no servidor
 console.log("Servidor iniciado na porta "+port+". Acesso em localhost:"+port);
