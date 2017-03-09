9fd6f89dd973b1fecc9ae7921c5f026f5806f240
Millena Kupsinsku Martins <millena.martins@digitaldesk.com.br>
2017-03-09 15:24:28 -0300

    criação da lista em arquivo json

---
+++ routes/list-users.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+var fs = require('fs');
+
+router.get('/list-users', function(req, res){//rota (localhost:porta/list-users)
+	fs.readFile(__dirname + '/../db/users.json', 'utf8', function(err, data){
+		res.end(data);
+	});
+});
+
+module.exports = router;
