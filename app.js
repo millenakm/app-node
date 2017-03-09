95f0a78922c61761536f5a1a728187088e916eda
Millena Kupsinsku Martins <millena.martins@digitaldesk.com.br>
2017-03-09 14:50:29 -0300

    adicionando rotas

--- app.js
+++ app.js
@@ -2,7 +2,12 @@ var express = require('express');
 var router = express.Router();//método do express
 
 var index = require('./routes/index');//chama o index
-
 router.use(index);//usa o index na router
+
+var users = require('./routes/users');
+router.use(users);
+
+var products = require('./routes/products');
+router.use(products);
 
 module.exports = router;//retorna o router para o servidor
