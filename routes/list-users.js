5f8639131884018386b0011210125fec14c11a6c
Millena Kupsinsku Martins <millena.martins@digitaldesk.com.br>
2017-03-10 09:36:58 -0300

    list-users fs

--- routes/list-users.js
+++ routes/list-users.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
+var file = require('./../controller/file');
 
 router.get('/list-users', function(req, res){//rota (localhost:porta/list-users)
 	fs.readFile(__dirname + '/../db/users.json', 'utf8', function(err, data){
